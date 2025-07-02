69c48c77cf60d37dd57cad0bbdb08766cf9c8689
Max ROUGEUX <max.rougeux2025@campus-eni.fr>
2025-07-02 11:51:10 +0200

    02-07 11h51

--- src/main/java/fr/eni/encheres/bo/Utilisateur.java
+++ src/main/java/fr/eni/encheres/bo/Utilisateur.java
@@ -1,10 +1,5 @@
 package fr.eni.encheres.bo;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
-import org.hibernate.validator.constraints.UniqueElements;
-
 import java.util.ArrayList;
 import java.util.List;
 
